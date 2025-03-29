c130b97295f0f8cd9a16c0cc5e8bf75b32144b4c
Artem Suprun <efogdev@gmail.com>
2025-03-29 23:30:53 +0200

    chore: checkpoint

--- main/ble/ble_hid_device.c
+++ main/ble/ble_hid_device.c
@@ -315,9 +315,9 @@ esp_err_t ble_hid_device_init(const bool verbose) {
     update_tx_power();
     vTaskDelay(1);
 
-    // if (has_saved_device()) {
-    //     connect_to_saved_device(get_gatts_if());
-    // }
+    if (has_saved_device()) {
+        connect_to_saved_device(get_gatts_if());
+    }
 
     return ESP_OK;
 }
