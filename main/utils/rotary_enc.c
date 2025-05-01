96de4e66e49976b7aa1185eefc9dd5a66f0b6e51
Artem Suprun <efogdev@gmail.com>
2025-05-01 12:44:43 +0300

    chore: checkpoint

--- main/utils/rotary_enc.c
+++ main/utils/rotary_enc.c
@@ -9,11 +9,10 @@
 #include "esp_log.h"
 
 #define PCNT_HIGH_LIMIT 8
-#define PCNT_LOW_LIMIT  -8
+#define PCNT_LOW_LIMIT  (-8)
 
 static QueueHandle_t enc_queue = NULL;
 static QueueHandle_t button_state_queue = NULL;
-static QueueHandle_t debug_queue = NULL;
 static rotary_callback_t user_callback = NULL;
 static rotary_click_callback_t user_click_callback = NULL;
 static rotary_long_press_callback_t user_long_press_callback = NULL;
@@ -24,10 +23,8 @@ static void rotary_enc_task(void* arg);
 static bool pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx)
 {
     BaseType_t high_task_wakeup = pdFALSE;
-    QueueHandle_t queue = (QueueHandle_t)user_ctx;
-    int8_t direction = edata->watch_point_value > 0 ? (edata->watch_point_value/(PCNT_HIGH_LIMIT/2)) : (edata->watch_point_value/(PCNT_LOW_LIMIT/2)*-1);
-    int value = edata->watch_point_value;
-    xQueueSendFromISR(debug_queue, &value, &high_task_wakeup);
+    const QueueHandle_t queue = (QueueHandle_t)user_ctx;
+    const int8_t direction = edata->watch_point_value > 0 ? (edata->watch_point_value/(PCNT_HIGH_LIMIT/2)) : (edata->watch_point_value/(PCNT_LOW_LIMIT/2)*-1);
     xQueueSendFromISR(queue, &direction, &high_task_wakeup);
     return (high_task_wakeup == pdTRUE);
 }
@@ -40,27 +37,26 @@ static void IRAM_ATTR click_isr_handler(void* arg) {
 void rotary_enc_init() {
     enc_queue = xQueueCreate(2, sizeof(int8_t));
     button_state_queue = xQueueCreate(2, sizeof(uint8_t));
-    debug_queue = xQueueCreate(2, sizeof(int));
 
-    pcnt_unit_config_t unit_config = {
+    const pcnt_unit_config_t unit_config = {
         .high_limit = PCNT_HIGH_LIMIT,
         .low_limit = PCNT_LOW_LIMIT,
     };
     ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
 
-    pcnt_glitch_filter_config_t filter_config = {
+    const pcnt_glitch_filter_config_t filter_config = {
         .max_glitch_ns = 5000,
     };
     ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
 
-    pcnt_chan_config_t chan_a_config = {
+    const pcnt_chan_config_t chan_a_config = {
         .edge_gpio_num = GPIO_ROT_A,
         .level_gpio_num = GPIO_ROT_B,
     };
     pcnt_channel_handle_t pcnt_chan_a = NULL;
     ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_a_config, &pcnt_chan_a));
 
-    pcnt_chan_config_t chan_b_config = {
+    const pcnt_chan_config_t chan_b_config = {
         .edge_gpio_num = GPIO_ROT_B,
         .level_gpio_num = GPIO_ROT_A,
     };
@@ -72,12 +68,12 @@ void rotary_enc_init() {
     ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
     ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
 
-    int watch_points[] = {PCNT_LOW_LIMIT, PCNT_LOW_LIMIT/2, PCNT_HIGH_LIMIT/2, PCNT_HIGH_LIMIT};
+    const int watch_points[] = {PCNT_LOW_LIMIT, PCNT_LOW_LIMIT/2, PCNT_HIGH_LIMIT/2, PCNT_HIGH_LIMIT};
     for (size_t i = 0; i < sizeof(watch_points) / sizeof(watch_points[0]); i++) {
         ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, watch_points[i]));
     }
 
-    pcnt_event_callbacks_t cbs = {
+    const pcnt_event_callbacks_t cbs = {
         .on_reach = pcnt_on_reach,
     };
     ESP_ERROR_CHECK(pcnt_unit_register_event_callbacks(pcnt_unit, &cbs, enc_queue));
@@ -86,7 +82,7 @@ void rotary_enc_init() {
     ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
     ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
 
-    gpio_config_t io_conf = {
+    const gpio_config_t io_conf = {
         .intr_type = GPIO_INTR_ANYEDGE,
         .mode = GPIO_MODE_INPUT,
         .pin_bit_mask = (1ULL << GPIO_ROT_E),
@@ -119,7 +115,6 @@ void rotary_enc_deinit() {
     gpio_isr_handler_remove(GPIO_ROT_E);
     vQueueDelete(enc_queue);
     vQueueDelete(button_state_queue);
-    vQueueDelete(debug_queue);
     user_callback = NULL;
     user_click_callback = NULL;
     user_long_press_callback = NULL;
@@ -141,11 +136,6 @@ static void rotary_enc_task(void* arg) {
                 user_callback(direction);
             }
             ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
-        }
-
-        int debug_value;
-        if (xQueueReceive(debug_queue, &debug_value, 0)) {
-            printf("\n%d\n", debug_value);
         }
 
         if (xQueueReceive(button_state_queue, &button_state, 0)) {
